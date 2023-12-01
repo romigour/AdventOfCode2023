8f52a460e5b5077adb7072e6d9f8e897cd164644
rgourmelen <romain.gourmelen@equasens.com>
2023-12-01 17:13:11 +0100

    DAY 1

--- src/main/java/Day1.java
+++ src/main/java/Day1.java
@@ -1,9 +1,8 @@
 import util.FileReader;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Day1 {
 
@@ -15,13 +14,44 @@ public class Day1 {
         System.out.println(reponse2(inputs));
     }
 
-    private static String reponse1(List<String> inputs) {
-
-        return null;
+    private static int reponse1(List<String> inputs) {
+        int sum = 0;
+        for (String input: inputs) {
+            String[] inputArray = input.replaceAll("[^\\d.]", "").split("");
+            sum += Integer.parseInt(inputArray[0] + inputArray[inputArray.length - 1]);
+        }
+        return sum;
     }
 
-    private static String reponse2(List<String> inputs) {
+    private static int reponse2(List<String> inputs) {
+        List<String> numbers = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+        int sum = 0;
+        for (String input: inputs) {
+            String output = "";
+            String[] arrayInput = input.split("");
+            for (int i = 0; i < input.length(); i++) {
+                if (isNumeric(arrayInput[i])) {
+                    output += arrayInput[i];
+                } else {
+                    for (int n = 0; n < numbers.size(); n++) {
+                        if (input.substring(i).startsWith(numbers.get(n))) {
+                            output += (n+1);
+                            break;
+                        }
+                    }
+                }
+            }
 
-        return null;
+            String[] outputArray = output.split("");
+            sum += Integer.parseInt(outputArray[0] + outputArray[outputArray.length - 1]);
+        }
+        return sum;
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        return Pattern.compile("-?\\d+(\\.\\d+)?").matcher(strNum).matches();
     }
 }
