6ee971ad8474be8288cfee4af1f69175d194594b
rgourmelen <romain.gourmelen@equasens.com>
2023-12-04 18:16:26 +0100

    DAY 4 - part 1

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,7 +1,7 @@
 import util.FileReader;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 public class Day4 {
 
@@ -9,17 +9,80 @@ public class Day4 {
 
         List<String> inputs = FileReader.read(4);
 
-        System.out.println(reponse1(inputs));
+        //System.out.println(reponse1(inputs));
         System.out.println(reponse2(inputs));
     }
 
-    private static String reponse1(List<String> inputs) {
+    private static int reponse1(List<String> inputs) {
 
-        return null;
+        int sum = 0;
+
+        for (String input: inputs) {
+            String[] arrayCards = input.split(" \\| ");
+
+            List<String> listCard1 = new ArrayList<>(Arrays.asList(arrayCards[0]
+                    .replaceAll("\\s{2,}", " ").trim()
+                    .replaceAll("Card -?\\d+(\\.\\d+)?: ", "").split(" ")));
+            List<String> listCard2 = new ArrayList<>(Arrays.asList(arrayCards[1].replaceAll("\\s{2,}", " ").trim().split(" ")));
+
+
+            listCard1.retainAll(listCard2);
+
+            int scoreParty = 0;
+            for (int i = 0; i < listCard1.size(); i++) {
+                if (i == 0) {
+                    scoreParty = 1;
+                } else {
+                    scoreParty = scoreParty * 2;
+                }
+            }
+            sum += scoreParty;
+        }
+
+        return sum;
     }
 
-    private static String reponse2(List<String> inputs) {
+    private static int reponse2(List<String> inputs) {
 
-        return null;
+        int sum = 0;
+
+        Map<Integer, Integer> mapCard = new HashMap<>();
+
+        for (int n = 1; n <= inputs.size(); n++) {
+            String[] arrayCards = inputs.get(n-1).split(" \\| ");
+
+            List<String> listCard1 = new ArrayList<>(Arrays.asList(arrayCards[0]
+                    .replaceAll("\\s{2,}", " ").trim()
+                    .replaceAll("Card -?\\d+(\\.\\d+)?: ", "").split(" ")));
+            List<String> listCard2 = new ArrayList<>(Arrays.asList(arrayCards[1].replaceAll("\\s{2,}", " ").trim().split(" ")));
+
+
+            listCard1.retainAll(listCard2);
+
+            for (int i = 1; i <= listCard1.size(); i++) {
+
+                if (mapCard.get((n + i)) == null) {
+                    if (mapCard.get(n) != null) {
+                        mapCard.put(n + i, 1 + mapCard.get(n));
+                    } else {
+                        mapCard.put(n + i, 1 + 1);
+                    }
+                } else {
+                    if (mapCard.get(n) != null) {
+                        mapCard.put(n + i, mapCard.get(n + i) + mapCard.get(n));
+                    } else {
+                        mapCard.put(n + i, mapCard.get(n + i) + 1);
+                    }
+                }
+
+                mapCard.putIfAbsent(n, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entrySet : mapCard.entrySet()) {
+            sum += entrySet.getValue();
+        }
+
+        return sum +1;
     }
 }
