5a6eec4cb378b1eb6334a1d8e6c2cd011b5d53f0
romi <romain.gourmelen@gmail.com>
2023-12-05 07:48:57 +0100

    Day 5 - part 1 (ok sur l'exemple mais résultat du puzzle faux)

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -1,7 +1,7 @@
 import util.FileReader;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 public class Day5 {
 
@@ -13,9 +13,40 @@ public class Day5 {
         System.out.println(reponse2(inputs));
     }
 
-    private static String reponse1(List<String> inputs) {
+    private static long reponse1(List<String> inputs) {
 
-        return null;
+        List<Long> seeds = Arrays.stream(inputs.get(0).replaceAll("seeds: ", "").split(" ")).map(Long::parseLong).toList();
+        List<String> path = new ArrayList<>();
+        List<List<Long>> lines = new ArrayList<>();
+        for (int i = 1; i < inputs.size(); i++) {
+            if (inputs.get(i).isEmpty()) {
+                List<Long> nextSeeds = new ArrayList<>();
+                for (long seed: seeds) {
+
+                    long newsSeed = -1;
+                    for (List<Long> line: lines) {
+                        if (seed >= line.get(1) && seed < (line.get(1) + line.get(2))) {
+                            newsSeed = line.get(0) - line.get(1) + seed;
+                            nextSeeds.add(newsSeed);
+                            break;
+                        }
+                    }
+
+                    if (newsSeed == -1L) {
+                        nextSeeds.add(seed);
+                    }
+                }
+                seeds = nextSeeds;
+                path.clear();
+                lines.clear();
+            } else if (inputs.get(i).contains(":")) {
+                path.addAll(Arrays.stream(inputs.get(i).split(" ")[0].split("-to-")).toList());
+            } else {
+                lines.add(Arrays.stream(inputs.get(i).split(" ")).map(Long::parseLong).toList());
+            }
+        }
+
+        return seeds.stream().mapToLong(v -> v).min().orElseThrow();
     }
 
     private static String reponse2(List<String> inputs) {
