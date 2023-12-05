f18fc0c7715043f19392ad8dbb2a101a4996091e
romi <romain.gourmelen@gmail.com>
2023-12-06 00:18:09 +0100

    Day 5 - part 1 Day 5 - part 2 (java heap)

--- src/main/java/Day5.java
+++ src/main/java/Day5.java
@@ -9,7 +9,7 @@ public class Day5 {
 
         List<String> inputs = FileReader.read(5);
 
-        System.out.println(reponse1(inputs));
+//        System.out.println(reponse1(inputs));
         System.out.println(reponse2(inputs));
     }
 
@@ -50,44 +50,50 @@ public class Day5 {
     private static long reponse2(List<String> inputs) {
         List<Long> rangeSeeds = Arrays.stream(inputs.get(0).replaceAll("seeds: ", "").split(" ")).map(Long::parseLong).toList();
 
-        List<Long> survivors = new ArrayList<>();
+        long survivor = Long.MAX_VALUE;
         for (int r = 0; r < rangeSeeds.size(); r = r+2) {
             List<Long> seeds = new ArrayList<>();
-            for (long j = rangeSeeds.get(r); j < (rangeSeeds.get(r) + rangeSeeds.get(r+1)); j++) {
-                seeds.add(j);
-            }
 
-            List<String> path = new ArrayList<>();
-            List<List<Long>> lines = new ArrayList<>();
-            for (int i = 1; i < inputs.size(); i++) {
-                if (inputs.get(i).isEmpty() || i == inputs.size()-1) {
-                    List<Long> nextSeeds = new ArrayList<>();
-                    for (long seed: seeds) {
+            System.out.println("Range [" + rangeSeeds.get(r) + " -> " + rangeSeeds.get(r+1) + "]");
+            for (long j = rangeSeeds.get(r); j < (rangeSeeds.get(r) + rangeSeeds.get(r+1)); j = j + 10000000) {
+                seeds.clear();
+                for (long k = j; k < (j + 10000000) && k < (rangeSeeds.get(r) + rangeSeeds.get(r+1)); k++) {
+                    seeds.add(k);
+                }
+                List<String> path = new ArrayList<>();
+                List<List<Long>> lines = new ArrayList<>();
+                for (int i = 1; i < inputs.size(); i++) {
+                    if (inputs.get(i).isEmpty() || i == inputs.size()-1) {
+                        List<Long> nextSeeds = new ArrayList<>();
+                        for (long seed: seeds) {
 
-                        long newsSeed = -1L;
-                        for (List<Long> line: lines) {
-                            if (seed >= line.get(1) && seed < (line.get(1) + line.get(2))) {
-                                newsSeed = line.get(0) - line.get(1) + seed;
-                                nextSeeds.add(newsSeed);
+                            long newsSeed = -1L;
+                            for (List<Long> line: lines) {
+                                if (seed >= line.get(1) && seed < (line.get(1) + line.get(2))) {
+                                    newsSeed = line.get(0) - line.get(1) + seed;
+                                    nextSeeds.add(newsSeed);
+                                }
+                            }
+
+                            if (newsSeed == -1L) {
+                                nextSeeds.add(seed);
                             }
                         }
-
-                        if (newsSeed == -1L) {
-                            nextSeeds.add(seed);
-                        }
+                        seeds = nextSeeds;
+                        path.clear();
+                        lines.clear();
+                    } else if (inputs.get(i).contains(":")) {
+                        path.addAll(Arrays.stream(inputs.get(i).split(" ")[0].split("-to-")).toList());
+                    } else {
+                        lines.add(Arrays.stream(inputs.get(i).split(" ")).map(Long::parseLong).toList());
                     }
-                    seeds = nextSeeds;
-                    path.clear();
-                    lines.clear();
-                } else if (inputs.get(i).contains(":")) {
-                    path.addAll(Arrays.stream(inputs.get(i).split(" ")[0].split("-to-")).toList());
-                } else {
-                    lines.add(Arrays.stream(inputs.get(i).split(" ")).map(Long::parseLong).toList());
                 }
+
+                long challenger = seeds.stream().mapToLong(v -> v).min().orElse(Long.MAX_VALUE);
+                if (survivor > challenger) survivor = challenger;
             }
-            survivors.add(seeds.stream().mapToLong(v -> v).min().orElse(Long.MAX_VALUE));
         }
 
-        return survivors.stream().mapToLong(v -> v).min().orElseThrow();
+        return survivor;
     }
 }
