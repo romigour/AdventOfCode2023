a473ef5c39b2416c40eddae0d89aa1bf83717b53
romi <romain.gourmelen@gmail.com>
2023-12-06 19:21:26 +0100

    Day 6

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -1,6 +1,8 @@
 import util.FileReader;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Day6 {
@@ -13,13 +15,73 @@ public class Day6 {
         System.out.println(reponse2(inputs));
     }
 
-    private static String reponse1(List<String> inputs) {
+    private static int reponse1(List<String> inputs) {
+        List<Integer> possibilities = new ArrayList<>();
+        List<Integer> times = Arrays.stream(inputs.get(0)
+                        .replaceAll("\\s{2,}", " ")
+                        .replace("Time: ", "").split(" "))
+                .map(Integer::parseInt).toList();
+        List<Integer> distances = Arrays.stream(inputs.get(1)
+                        .replaceAll("\\s{2,}", " ")
+                        .replace("Distance: ", "").split(" "))
+                .map(Integer::parseInt).toList();
 
-        return null;
+        for (int occ = 0; occ < distances.size(); occ++) {
+            int possibility = 0;
+            for (int t = 0; t < times.get(occ); t++) {
+                int vitesse = t;
+                int tempsRestant = times.get(occ) - t;
+                int distanceParcouru = vitesse * tempsRestant;
+
+                if (distances.get(occ) < distanceParcouru) {
+                    possibility++;
+                }
+            }
+            possibilities.add(possibility);
+         }
+
+
+        int sum = 1;
+
+        for (int possibility : possibilities) {
+            sum *= possibility;
+        }
+
+        return sum;
     }
 
-    private static String reponse2(List<String> inputs) {
+    private static int reponse2(List<String> inputs) {
+        List<Long> possibilities = new ArrayList<>();
+        List<Long> times = Arrays.stream(inputs.get(0)
+                        .replaceAll("\\s{2,}", " ")
+                        .replace("Time: ", "").replaceAll(" ", "").split(" "))
+                .map(Long::parseLong).toList();
+        List<Long> distances = Arrays.stream(inputs.get(1)
+                        .replaceAll("\\s{2,}", " ")
+                        .replace("Distance: ", "").replaceAll(" ", "").split(" "))
+                .map(Long::parseLong).toList();
 
-        return null;
+        for (int occ = 0; occ < distances.size(); occ++) {
+            long possibility = 0;
+            for (int t = 0; t < times.get(occ); t++) {
+                int vitesse = t;
+                long tempsRestant = times.get(occ) - t;
+                long distanceParcouru = vitesse * tempsRestant;
+
+                if (distances.get(occ) < distanceParcouru) {
+                    possibility++;
+                }
+            }
+            possibilities.add(possibility);
+        }
+
+
+        int sum = 1;
+
+        for (long possibility : possibilities) {
+            sum *= possibility;
+        }
+
+        return sum;
     }
 }
