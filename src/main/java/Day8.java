0095a3fd539ffa9fe20d624bd74aaefd341822f4
romi <romain.gourmelen@gmail.com>
2023-12-09 16:54:36 +0100

    Day 8 - part 1 Day 9 - part 1 & 2

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -1,7 +1,11 @@
 import util.FileReader;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Day8 {
 
@@ -9,17 +13,102 @@ public class Day8 {
 
         List<String> inputs = FileReader.read(8);
 
-        System.out.println(reponse1(inputs));
+        //System.out.println(reponse1(inputs));
         System.out.println(reponse2(inputs));
     }
 
+
     private static int reponse1(List<String> inputs) {
 
-        return 0;
+        Map<String, Node> mapNodes = new HashMap<>();
+
+        List<String> instructions = Arrays.asList(inputs.get(0).split(""));
+
+
+        for (int i = 2; i < inputs.size(); i++) {
+            // AAA = (BBB, CCC)
+            String[] array1 = inputs.get(i).split(" = ");
+            String[] array2 = array1[1].replace("(", "").replace(")", "").split(", ");
+
+            mapNodes.put(array1[0], new Node(array2[0], array2[1]));
+        }
+
+        boolean zzzFound = false;
+
+
+        String current = "AAA";
+
+        int nbStep = 0;
+        while (!zzzFound) {
+            for (String instruction : instructions) {
+                if ("L".equals(instruction)) {
+                    current = mapNodes.get(current).left;
+                } else {
+                    current = mapNodes.get(current).right;
+                }
+
+                nbStep++;
+                if ("ZZZ".equals(current)) {
+                    zzzFound = true;
+                    break;
+                }
+            }
+        }
+
+
+        return nbStep;
     }
 
     private static int reponse2(List<String> inputs) {
 
-        return 0;
+        Map<String, Node> mapNodes = new HashMap<>();
+
+        List<String> instructions = Arrays.asList(inputs.get(0).split(""));
+
+
+        for (int i = 2; i < inputs.size(); i++) {
+            // AAA = (BBB, CCC)
+            String[] array1 = inputs.get(i).split(" = ");
+            String[] array2 = array1[1].replace("(", "").replace(")", "").split(", ");
+
+            mapNodes.put(array1[0], new Node(array2[0], array2[1]));
+        }
+
+        boolean zzzFound = false;
+
+
+        List<String> currents = mapNodes.keySet().stream().filter(s -> s.endsWith("A")).collect(Collectors.toList());
+
+        int nbStep = 0;
+        while (!zzzFound) {
+            for (String instruction : instructions) {
+                for (int i = 0; i < currents.size(); i++) {
+                    if ("L".equals(instruction)) {
+                        currents.set(i, mapNodes.get(currents.get(i)).left);
+                    } else {
+                        currents.set(i, mapNodes.get(currents.get(i)).right);
+                    }
+                }
+
+                nbStep++;
+                if (currents.stream().allMatch(s -> s.endsWith("Z"))) {
+                    zzzFound = true;
+                    break;
+                }
+            }
+        }
+
+
+        return nbStep;
+    }
+
+    static class Node {
+        String left;
+        String right;
+
+        public Node(String left, String right) {
+            this.left = left;
+            this.right = right;
+        }
     }
 }
--- src/main/java/Day9.java
+++ src/main/java/Day9.java
@@ -1,7 +1,8 @@
 import util.FileReader;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day9 {
 
@@ -15,11 +16,75 @@ public class Day9 {
 
     private static int reponse1(List<String> inputs) {
 
-        return 0;
+        List<List<Integer>> historiques = new ArrayList<>();
+
+        for (String input: inputs) {
+            historiques.add(Arrays.asList(input.split(" ")).stream().map(Integer::parseInt).collect(Collectors.toList()));
+        }
+        int sum = 0;
+        for (List<Integer> historique: historiques) {
+            List<List<Integer>> sousHistoriques = new ArrayList<>();
+            sousHistoriques.add(historique);
+            boolean end = false;
+            while (!end) {
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < sousHistoriques.get(sousHistoriques.size() - 1).size() - 1; i++) {
+                    list.add(sousHistoriques.get(sousHistoriques.size() - 1).get(i + 1) - sousHistoriques.get(sousHistoriques.size() - 1).get(i));
+                }
+                sousHistoriques.add(list);
+
+                if (list.stream().allMatch(i -> i == 0)) {
+                    end = true;
+                }
+            }
+            for (int j = sousHistoriques.size() - 1; j >= 0; j--) {
+                if (j == (sousHistoriques.size() - 1)) {
+                    sousHistoriques.get(j).add(0);
+                } else if (j == 0) {
+                    sum += sousHistoriques.get(j).get(sousHistoriques.get(j).size() - 1) + sousHistoriques.get(j+1).get(sousHistoriques.get(j).size() - 1);
+                } else {
+                    sousHistoriques.get(j).add(sousHistoriques.get(j).get(sousHistoriques.get(j).size() - 1) + sousHistoriques.get(j+1).get(sousHistoriques.get(j).size() - 1));
+                }
+            }
+        }
+
+        return sum;
     }
 
     private static int reponse2(List<String> inputs) {
 
-        return 0;
+        List<Deque<Integer>> historiques = new ArrayList<>();
+
+        for (String input: inputs) {
+            historiques.add(Arrays.stream(input.split(" ")).map(Integer::parseInt).collect(Collectors.toCollection(ArrayDeque::new)));
+        }
+        int sum = 0;
+        for (Deque<Integer> historique: historiques) {
+            List<Deque<Integer>> sousHistoriques = new ArrayList<>();
+            sousHistoriques.add(historique);
+            boolean end = false;
+            while (!end) {
+                Deque<Integer> list = new ArrayDeque<>();
+                for (int i = 0; i < sousHistoriques.get(sousHistoriques.size() - 1).size() - 1; i++) {
+                    list.add(sousHistoriques.get(sousHistoriques.size() - 1).stream().toList().get(i + 1) - sousHistoriques.get(sousHistoriques.size() - 1).stream().toList().get(i));
+                }
+                sousHistoriques.add(list);
+
+                if (list.stream().allMatch(i -> i == 0)) {
+                    end = true;
+                }
+            }
+            for (int j = sousHistoriques.size() - 1; j >= 0; j--) {
+                if (j == (sousHistoriques.size() - 1)) {
+                    sousHistoriques.get(j).addFirst(0);
+                } else if (j == 0) {
+                    sum += sousHistoriques.get(j).getFirst() - sousHistoriques.get(j+1).getFirst();
+                } else {
+                    sousHistoriques.get(j).addFirst(sousHistoriques.get(j).getFirst() - sousHistoriques.get(j+1).getFirst());
+                }
+            }
+        }
+
+        return sum;
     }
 }
