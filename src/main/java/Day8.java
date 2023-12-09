e81203686b2382f24202b7b27fd67c64721d84ad
romi <romain.gourmelen@gmail.com>
2023-12-09 19:04:15 +0100

    Day 8 - part 2

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -1,11 +1,9 @@
 import util.FileReader;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Day8 {
 
@@ -59,11 +57,11 @@ public class Day8 {
         return nbStep;
     }
 
-    private static int reponse2(List<String> inputs) {
+    private static long reponse2(List<String> inputs) {
 
         Map<String, Node> mapNodes = new HashMap<>();
 
-        List<String> instructions = Arrays.asList(inputs.get(0).split(""));
+        String[] instructions = inputs.get(0).split("");
 
 
         for (int i = 2; i < inputs.size(); i++) {
@@ -74,32 +72,49 @@ public class Day8 {
             mapNodes.put(array1[0], new Node(array2[0], array2[1]));
         }
 
-        boolean zzzFound = false;
-
 
         List<String> currents = mapNodes.keySet().stream().filter(s -> s.endsWith("A")).collect(Collectors.toList());
 
-        int nbStep = 0;
-        while (!zzzFound) {
-            for (String instruction : instructions) {
-                for (int i = 0; i < currents.size(); i++) {
+        List<Integer> listNbStep = new ArrayList<>();
+
+        for (int i = 0; i < currents.size(); i++) {
+            int nbStep = 0;
+            boolean zzzFound = false;
+            while (!zzzFound) {
+                for (String instruction : instructions) {
                     if ("L".equals(instruction)) {
                         currents.set(i, mapNodes.get(currents.get(i)).left);
                     } else {
                         currents.set(i, mapNodes.get(currents.get(i)).right);
                     }
-                }
+                    nbStep++;
 
-                nbStep++;
-                if (currents.stream().allMatch(s -> s.endsWith("Z"))) {
-                    zzzFound = true;
-                    break;
+                    if (currents.get(i).endsWith("Z")) {
+                        zzzFound = true;
+                        listNbStep.add(nbStep);
+                        break;
+                    }
                 }
             }
         }
 
+        return listNbStep.stream().mapToLong(a -> a).reduce(Day8::lcm).getAsLong();
+    }
 
-        return nbStep;
+    // https://www.baeldung.com/java-least-common-multiple
+    public static long lcm(long number1, long number2) {
+        if (number1 == 0 || number2 == 0) {
+            return 0;
+        }
+        long absNumber1 = Math.abs(number1);
+        long absNumber2 = Math.abs(number2);
+        long absHigherNumber = Math.max(absNumber1, absNumber2);
+        long absLowerNumber = Math.min(absNumber1, absNumber2);
+        long lcm = absHigherNumber;
+        while (lcm % absLowerNumber != 0) {
+            lcm += absHigherNumber;
+        }
+        return lcm;
     }
 
     static class Node {
